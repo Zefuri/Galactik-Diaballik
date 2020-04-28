80097d9787af5ab444d51bd5aed49af5e6dab529
folet14 <jed14@orange.fr>
2020-04-28 12:33:11 +0200

    C is not good

--- model/Player.java
+++ model/Player.java
@@ -3,12 +3,30 @@ package model;
 public class Player {
 	private String team;
 	private boolean ballPossession;
-	
+	private int i;
+	private int j;
+
 	public Player(String team) {
 		this.team = team;
 		this.ballPossession = false;
 	}
-	
+
+	public Player(String team, int x, int y) {
+		this.team = team;
+		this.ballPossession = false;
+		this.i = x;
+		this.j = y;
+	}
+
+	public int getI(){return this.i;}
+
+	public int getJ(){return this.j;}
+
+	public void movePlayer(int x, int y){
+		this.i = x;
+		this.j = y;
+	}
+
 	public String getTeam() {
 		return team;
 	}
