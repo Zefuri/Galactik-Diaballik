4ae027c598ee5934d379564d66bdacf7db412d20
Benjass <lafond.benjamin@free.fr>
2020-05-15 17:17:34 +0200

    remise des new dans les creation d'objet

--- model/Player.java
+++ model/Player.java
@@ -92,7 +92,7 @@ public class Player {
 		if (canMove(direction)) {
 			Case newPosition = calculateNewPosition(direction);
 
-			Action moveAction = new Action(ActionType.MOVE, this, this, this.position, newPosition);
+			Action moveAction = new Action(ActionType.MOVE, this, this, new Case(this.position), newPosition);
 			
 			return this.getStadium().actionPerformed(moveAction);
 		} else {
@@ -107,7 +107,7 @@ public class Player {
 	
 	public ActionResult pass(Player nextPlayer) {
 		if (canPass(nextPlayer)) {
-			Action passAction = new Action(ActionType.PASS, this, nextPlayer, this.position, nextPlayer.getPosition());
+			Action passAction = new Action(ActionType.PASS, this, nextPlayer, new Case(this.position), nextPlayer.getPosition());
 		
 			return this.getStadium().actionPerformed(passAction);
 		} else {
