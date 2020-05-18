9d7ecaebeeef6753f89b0a77be79f0ec1ed80587
Benjass <lafond.benjamin@free.fr>
2020-05-18 15:51:38 +0200

    mise en retrait du minmax et des ai oublié

---
+++ controller/ai/artefact/PlayerType.java
@@ -0,0 +1,29 @@
+package controller.ai;
+
+import model.Stadium;
+import model.Team;
+import model.enums.TeamPosition;
+
+//import model.Player;
+//
+//// Common class for all player: ai or human
+public abstract class PlayerType {
+
+	Team team;
+	Stadium stadium;
+
+	public PlayerType(Stadium stadium, TeamPosition position) {
+		this.stadium = stadium;
+		team = this.stadium.getTeam(position);
+	}	
+	
+	//For ai
+	boolean timeOut() {
+		return false;
+	}
+
+	//For human
+	boolean play(int abs, int ord) {
+		return false;
+	}
+}
